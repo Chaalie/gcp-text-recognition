fb2f6995ed5230b5fe28b86e7ae81c79ab7ff6d9
Karol Waszczuk <waszczukarol@gmail.com>
2021-05-11 17:31:07 +0200

    Restructurize image-transformer gcf

--- cloud-functions/image-transformer/src/main.py
+++ cloud-functions/image-transformer/src/main.py
@@ -10,16 +10,16 @@ MIN_WIDTH = 1024
 MIN_HEIGHT = 768
 
 def main(file_data, context):
-    src_file_name = data["name"]
-    src_bucket_name = data["bucket"]
+    src_file_name = file_data["name"]
+    src_bucket_name = file_data["bucket"]
 
     print(f'processing: gs://{src_bucket_name}/{src_file_name}')
 
     src_blob = storage_client.bucket(src_bucket_name).get_blob(src_file_name)
-    _, tmp_file = tempfile.mkstemp()
-    src_blob.download_to_filename(tmp_file)
+    _, tmp_filename = tempfile.mkstemp()
+    src_blob.download_to_filename(tmp_filename)
 
-    with Image(filename=tmp_file) as image:
+    with Image(filename=tmp_filename) as image:
         ratio = image.width / image.height
         scaled_width = max(image.width, MIN_WIDTH)
         scaled_height = max(image.height, MIN_HEIGHT)
@@ -32,7 +32,7 @@ def main(file_data, context):
 
         image.format = 'jpeg'
         image.resize(scaled_width, scaled_height)
-        image.save(filename=tmp_file)
+        image.save(filename=tmp_filename)
 
     dest_file_name = src_file_name
     dest_bucket_name = os.getenv('DESTINATION_BUCKET')
@@ -41,5 +41,7 @@ def main(file_data, context):
 
     dest_bucket = storage_client.bucket(dest_bucket_name)
     dest_blob = dest_bucket.blob(dest_file_name)
-    dest_blob.upload_from_filename(tmp_file)
+    dest_blob.upload_from_filename(tmp_filename, content_type='jpeg')
+
+    os.remove(tmp_filename)
     
