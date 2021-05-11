df02dfed155abc565d50b69347e1d55e84a0a65f
Karol Waszczuk <waszczukarol@gmail.com>
2021-05-11 17:59:57 +0200

    Text-recognizer gcd initial implementation

---
+++ cloud-functions/text-recognizer/src/main.py
@@ -0,0 +1,34 @@
+import os
+
+from google.cloud import storage, vision, pubsub_v1
+
+storage_client = storage.Client()
+vision_client = vision.ImageAnnotatorClient()
+publisher = pubsub_v1.PublisherClient()
+
+PROJECT_ID = os.environ.get('GCP_PROJECT')
+TOPIC_NAME = os.environ.get('TOPIC_NAME')
+
+def main(file_data, context):
+    src_file_name = file_data["name"]
+    src_bucket_name = file_data["bucket"]
+
+    print(f'processing: gs://{src_bucket_name}/{src_file_name}')
+
+    image = vision.Image(
+        source=vision.ImageSource(gcs_image_uri=f"gs://{src_bucket_name}/{src_file_name}")
+    )
+    text_detection = vision_client.text_detection(image=image)
+    annotations = text_detection.text_annotations
+
+    if len(annotations) > 0:
+        found_text = annotations[0].description
+        print('Text detected:', found_text)
+    else:
+        found_text = None
+        print('No text detected')
+
+    message = src_file_name
+    message_data = message.encode("utf-8")
+    topic_path = publisher.topic_path(PROJECT_ID, TOPIC_NAME)
+    publisher.publish(topic_path, data=message_data).result()
